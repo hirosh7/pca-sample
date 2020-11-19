e50fab5df88ef7645cae19a07378258082ec2735
Keith W. Johnson <hirosh7@gmail.com>
2020-11-18 23:59:47 -0700

    Create sample-swift.swift

---
+++ sample-swift.swift
@@ -0,0 +1,16 @@
+// Initialize Array with 5 elements
+var myArray = [1, 2 , 3, 4, 5]
+// Remove all elements from an array
+myArray.removeAll()
+// Check if Array is empty
+if myArray.isEmpty {
+    print("Array is empty")
+} else {
+    print("Array is not empty")
+}
+// Another way to check if Array is empty
+if myArray.count > 0 {
+    print("Array is empty")
+} else {
+    print("Array is not empty")
+}
